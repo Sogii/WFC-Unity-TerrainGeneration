ac9cf3f0acc5453f061becc14a18c90917c6f451
Sogii <s.p.v.lindt@student.tue.nl>
2023-03-30 17:17:49 +0200

    working demo script

--- Assets/Scripts/RiverGenerator.cs
+++ Assets/Scripts/RiverGenerator.cs
@@ -9,20 +9,30 @@ public class RiverGenerator : MonoBehaviour
     public GameObject Prefab;
     public GameObject SplineHolder;
     [SerializeField] private int _stepCount;
+    [SerializeField] private float _riverWidth;
     private float  _pathLength;
     private float stepSize;
+    private VertexPath _riverPath;
     private PathCreator pathCreator;
 
     private void Start(){
         pathCreator = this.gameObject.GetComponent<PathCreator>();
-        VertexPath riverPath = pathCreator.path;
-        _pathLength = riverPath.length;
-        float stepSize = _pathLength/_stepCount;
+        _riverPath = pathCreator.path;
+        _pathLength = _riverPath.length;
+        stepSize = _pathLength/_stepCount;
+        GenerateRiverMesh();
     }
 
     
     private void GenerateRiverMesh(){
-        
+        float dst = 0;
+        while(dst < _riverPath.length){
+            Vector3 point = _riverPath.GetPointAtDistance(dst);
+            Instantiate(Prefab, point, Prefab.transform.rotation ,SplineHolder.transform);
+            dst += stepSize; 
+        }   
     }
+
+    
 
 }
